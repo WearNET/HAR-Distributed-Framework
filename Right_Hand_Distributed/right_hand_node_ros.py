62e0bfd448b58ff4520814ea9e1267a8dd0a2683
jhorivas <jhorivas0997@gmail.com>
2026-01-16 17:11:57 -0400

    Adding Start/Stop Logic and Latest Sample Window for Metrics

--- Right_Hand_Distributed/right_hand_node_ros.py
+++ Right_Hand_Distributed/right_hand_node_ros.py
@@ -15,7 +15,7 @@ import joblib
 # ROS
 import rospy
 from har_msgs.msg import Probs  # generado en har_msgs/msg/Probs.msg
-from std_msgs.msg import Header, Empty
+from std_msgs.msg import Header, Empty, Bool
 from sensor_msgs.msg import Imu
 
 # ===================== PARÁMETROS (EDITA AQUÍ) =====================
@@ -92,12 +92,11 @@ class RHandNode:
         self.pub_quat = rospy.Publisher(TOPIC_QUAT, Imu, queue_size=10)
         self.seq = 0
 
-        # --- START TOPICS SAME TIME ---
-        self.started = False              
-        self.t0_experiment_ns = None
-        self.start_sub = rospy.Subscriber(
+        # # --- START TOPICS SAME TIME ---
+        self.started = False
+        self.state_sub = rospy.Subscriber(
             "/har/start",
-            Empty,
+            Bool,
             self._start_cb,
             queue_size=1
         )
@@ -198,13 +197,18 @@ class RHandNode:
             self.dev.disconnect()
             rospy.loginfo(f"[{self.sensor_id}] Desconectado.")
 
-    # ---------- Callback de inicio (/har/start) ----------
-    def _start_cb(self, msg: Empty):
-        self.started = True
-        self.t0_experiment_ns = rospy.Time.now().to_nsec()
-        self.seq = 0
-        self.buffer.clear()
-        rospy.loginfo(f"[{self.sensor_id}] START recibido -> t0_experiment_ns={self.t0_experiment_ns}")
+    # # ---------- Callback de inicio (/har/start) ----------
+    def _start_cb(self, msg: Bool):
+        if msg.data:
+            # START
+            self.started = True
+            self.seq = 0
+            self.buffer.clear()
+            rospy.loginfo(f"[{self.sensor_id}] RUN=True (START) recibido")
+        else:
+            # STOP
+            self.started = False
+            rospy.loginfo(f"[{self.sensor_id}] RUN=False (STOP) recibido")
 
     # ---------- Callbacks ----------
     def _cb_cacc(self, ctx: c_void_p, data: c_void_p):
@@ -273,7 +277,7 @@ class RHandNode:
                 t0 = time.time()
                 if len(self.buffer) >= self.window_size:
                     window = list(self.buffer)[-self.window_size:]            # [(ts, [6]), ...] × 50
-                    t0_ns = window[0][0]
+                    t0_ns = window[-1][0]
                     X = np.array([s for (_, s) in window], dtype=np.float32)  # (50, 6)
 
                     # Escalado (6 columnas [ax,ay,az,gx,gy,gz])
